fee6ac92e2f87e11707f5b8de936b7bebdef3fd6
Julien ichard WCS <julien.y.richard@gmail.com>
2022-09-09 15:17:40 +0200

    Nuxt tutorial 4

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import { defineNuxtConfig } from 'nuxt';
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  publicRuntimeConfig: {
+    WEATHER_APP_KEY: process.env.WEATHER_APP_KEY
+  },
+  privateRuntimeConfig: {},
+  css: [
+    "~/assets/css/tailwind.css"
+  ],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {}
+        }
+      }
+    }
+  }
+});
